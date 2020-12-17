486f548ae510c249fb8e72c4bc830e3edc3c1d76
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2020-12-17 22:59:33 +0800

    introduced memoization for cart selector

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,24 @@
+import { createSelector } from 'reselect'
+
+//input selector - function that usually takes below structure. It gets the whole state and returns a slice of it
+//in below example, it gets selectCart from state
+const selectCart = state => state.cart
+//const selectCartItems = state => state.selectCartItems
+
+export const selectCartItems = createSelector( //since we used createSelector, it is now eh memoized selector
+    [selectCart], //collection of input selectors
+    cart => {
+        console.log('carItem console log', cart.cartItems)
+        return cart.cartItems
+    } //function that will return the value we want out of the selector
+)
+
+ export const selectCartItemsCount = createSelector(
+     [selectCartItems],
+     cartItems => 
+        cartItems.reduce(
+            (accumulatedQuantity, cartItem) => 
+                accumulatedQuantity + cartItem.quantity,
+                0
+        )
+ )
